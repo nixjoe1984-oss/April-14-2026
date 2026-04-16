c0c9b769361220958f0b9ca7f64166ff9f09772b
nixjoe1984-oss <nixjoe1984@gmail.com>
2026-04-15 21:30:47 -0400

    Add files via upload

---
+++ Afterclass.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a whole number:"))
+c=0
+t=num
+if num <= 9 and num >= 0:
+    c=1
+else:
+    while t > 0:
+        c+=1
+        t //=10
+print("The length of", num, "is", c)
